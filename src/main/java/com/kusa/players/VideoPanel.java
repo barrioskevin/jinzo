0c45ca4bbe8ec057aefdb6bdd36f232151c0ceed
Kevin Barrios <barrik3@unlv.nevada.edu>
2025-02-23 02:51:20 -0800

    made video panel play a playlist and schedule the playlist updater

--- src/main/java/com/kusa/players/VideoPanel.java
+++ src/main/java/com/kusa/players/VideoPanel.java
@@ -1,18 +1,40 @@
 package com.kusa.players;
 
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.player.base.MediaPlayer;
+import uk.co.caprica.vlcj.player.base.events.MediaPlayerEvent;
+import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
+import com.kusa.service.LocalService;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Timer;
+import com.kusa.PlaylistUpdaterTask;
+
 
 public class VideoPanel extends EmbeddedMediaPlayerComponent
 {
   EmbeddedMediaPlayerComponent component;
+  private Playlist playlist;
   
   public VideoPanel()
   {
+    playlist = new Playlist();
     setOpaque(true);
+    mediaPlayer().events().addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
+      @Override
+      public void finished(MediaPlayer mp)
+      {
+        mediaPlayer().submit(() -> mediaPlayer().media().play(playlist.next()));
+      }
+    });
+
+    Timer t = new Timer();
+    t.schedule(new PlaylistUpdaterTask(playlist), 1000, 60000);
   }
 
-  public void playVideo(String path)
+  public void play()
   {
-    mediaPlayer().media().play(path);
+    mediaPlayer().media().play(playlist.next());
   }
+
 }
