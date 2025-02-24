8557dd7db2d28d88cf22f1789fc424e7284ba6d5
Kevin Barrios <barrik3@unlv.nevada.edu>
2025-02-23 20:29:00 -0800

    documentation describing how panels are used

--- src/main/java/com/kusa/players/SidePanel.java
+++ src/main/java/com/kusa/players/SidePanel.java
@@ -17,6 +17,13 @@ import javax.swing.ImageIcon;
 import javax.swing.BorderFactory;
 import java.awt.GridLayout;
 
+/**
+ * Class for managing a side panel of an engagment frame.
+ *
+ * right now this class only works by displaying an image from
+ * a path specified in the constructor. in the future i want to extend
+ * functionality to allow for creating panels with custom text as well.
+ */
 public class SidePanel extends JPanel
 {
   private BufferedImage originalImage;
@@ -24,10 +31,24 @@ public class SidePanel extends JPanel
   private ImageIcon icon;
   private JLabel imageLabel;
 
-  //default to 660x1080
+  /**
+   * Constructs a side panel with the default dimensions.
+   * default dimensions attempt to take up 1/3 of the 1080p display.
+   * @param path path of image content to be displayed.
+   */ 
   public SidePanel(String path) { this(path, 640, 1080); }
+  /**
+   * Constucts a side panel with custom dimensions.
+   * we can specify our own width and height but it's up to the engament frame to 
+   * determine how it will be displayed in the end.
+   * @param path path of image content to be displayed.
+   * @param width int representing width of side panel.
+   * @param height int representing height of side panel.
+   */
   public SidePanel(String path, int width, int height)
   {
+    //TODO Handle failure properly!!!
+      
     setLayout(new BorderLayout());
 
     try{
@@ -51,6 +72,17 @@ public class SidePanel extends JPanel
     add(imageLabel, BorderLayout.CENTER); 
   }
 
+  /**
+   * Sets the content displayed on the side panel.
+   *
+   * if it fails to load the new image it will 
+   * just stick to the image it currently has.
+   *
+   * we have a Task for using this function i just havent
+   * found the best place to use it.
+   * 
+   * @param path path of new image to be displayed. 
+   */
   public void setImage(String path)
   {
     BufferedImage newImage;
--- src/main/java/com/kusa/players/VideoPanel.java
+++ src/main/java/com/kusa/players/VideoPanel.java
@@ -11,15 +11,30 @@ import java.util.Timer;
 import com.kusa.PlaylistUpdaterTask;
 
 
+/**
+ * Class representing the video content in an engagment frame.
+ *
+ * this class extends vlcjs embedded media player component for
+ * access to vlc bindings.
+ */
 public class VideoPanel extends EmbeddedMediaPlayerComponent
 {
-  EmbeddedMediaPlayerComponent component;
   private Playlist playlist;
   
+  /**
+   * Constructs a video panel for use in an engagment frame.
+   *
+   * we should proably add the playlist as a parameter to this 
+   * class eventually so we don't have to manage the playlist 
+   * within a video panel.
+   */
   public VideoPanel()
   {
     playlist = new Playlist();
     setOpaque(true);
+
+    //we setup a event adapter to automatically play the next video in a playlist
+    //once a video ends.
     mediaPlayer().events().addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
       @Override
       public void finished(MediaPlayer mp)
@@ -28,10 +43,21 @@ public class VideoPanel extends EmbeddedMediaPlayerComponent
       }
     });
 
+    //we also start a task here for managing our playlist
+    //TODO this is probably not the best place to do this!!!
     Timer t = new Timer();
     t.schedule(new PlaylistUpdaterTask(playlist), 1000, 60000);
   }
 
+  /**
+   * Plays the current media.
+   *
+   * called by the main app to start the video panel. 
+   *
+   * because the video panel takes care of starting new videos 
+   * after one ends this method should only be called once 
+   * after the engagment frame has been created.
+   */
   public void play()
   {
     mediaPlayer().media().play(playlist.next());
