53116fe2aa503341d1f5288f417e39e0106a15ef
Kevin Barrios <kusakus@live.com>
2025-02-24 17:11:32 -0800

    added file exists method + updated class to use full paths

--- src/main/java/com/kusa/service/LocalService.java
+++ src/main/java/com/kusa/service/LocalService.java
@@ -7,16 +7,34 @@ import java.util.HashSet;
 public class LocalService
 {
 
-  public static Set<String> getDownloadedVideoNames()
+  public static final String videosPath = Config.getProperty("downloadPath") + "videos/";
+
+  /**
+   * Returns true if file exists on the local machine.
+   *
+   * @param fullName full path of file being checked.
+   */
+  public static boolean fileExists(String fullName)
   {
-    File dir = new File(Config.getProperty("downloadPath") + "videos/");
+    File file = new File(fullName);
+    return file.exists();
+  } 
+
+  /**
+   * Returns list of paths for all the downloaded videos in our applications media folder.
+   *
+   * @return set of absolute paths for each file in apps media/videos folder.
+   */
+  public static Set<String> getVideoMRLS()
+  {
+    File dir = new File(videosPath);
     File[] files = dir.listFiles();
     Set<String> names = new HashSet<>();
     if (files != null)
     {
       for(File file : files)
       {
-        names.add(file.getName());
+        names.add(file.getAbsolutePath());
       }
     }
     return names;
