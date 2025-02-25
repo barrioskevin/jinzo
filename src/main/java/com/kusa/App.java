8fe5b559a4c55247265c657c0a3b7cd969ed82be
Kevin Barrios <kusakus@live.com>
2025-02-24 20:36:54 -0800

    download resources on start up. create playlist. start tasks. finally launch engagment player

--- src/main/java/com/kusa/App.java
+++ src/main/java/com/kusa/App.java
@@ -1,5 +1,6 @@
 package com.kusa;
 import com.kusa.player.AppFrame;
+import com.kusa.playlist.CircularQueuePlaylist;
 
 import com.kusa.service.GDriveService;
 import com.kusa.service.LocalService;
@@ -7,7 +8,10 @@ import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
 
 import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Timer;
 
 
@@ -28,10 +32,43 @@ public class App
   {
     GDriveService gds = new GDriveService();
 
-    new AppFrame();
+    //download photos.
+    List<File> photos = gds.getPhotoFiles();
+    Set<String> localPhotos = LocalService.getPhotoMRLS();
+    Set<String> drivePhotos = new HashSet<>();
+    for(File photo : photos)
+    {
+      String p = LocalService.photosPath + photo.getName();
+      drivePhotos.add(p);
+      if(!localPhotos.contains(p))
+        gds.downloadFile(photo.getId(), "photos/");
+    }
+    //download videos.
+    List<File> videos = gds.getVideoFiles();
+    Set<String> localVideos = LocalService.getVideoMRLS();
+    Set<String> driveVideos = new HashSet<>();
+    for(File video : videos)
+    {
+      String p = LocalService.videosPath + video.getName();
+      driveVideos.add(p);
+      if(!localVideos.contains(p))
+        gds.downloadFile(video.getId(), "videos/");
+    }
 
+    //create playlist.
+    Set<String> intersectingVideos = new HashSet<>(localVideos);
+    intersectingVideos.retainAll(driveVideos);
+    CircularQueuePlaylist cqp = new CircularQueuePlaylist(new ArrayList<String>(intersectingVideos));
+
+    //set up playlist sync task.
+    Timer playlistTimer = new Timer();
+    playlistTimer.schedule(new PlaylistDriveSyncTask(cqp, gds), 1000, 60000);
+
+    //set up drive video downloader
     Timer downloadTimer = new Timer();
     downloadTimer.schedule(new VideoDownloaderTask(gds), 1000, 60000);
+
+    new AppFrame(cqp);
 
     System.out.println(Config.getProperty("downloadPath"));
     System.out.println(Config.getProperty("tokenStoragePath"));
