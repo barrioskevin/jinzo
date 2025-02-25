45dfa589a30ef6124e7ef67212f10919a27bedaa
Kevin Barrios <kusakus@live.com>
2025-02-24 20:34:39 -0800

    download task only call service if file not present

--- src/main/java/com/kusa/VideoDownloaderTask.java
+++ src/main/java/com/kusa/VideoDownloaderTask.java
@@ -1,10 +1,14 @@
 package com.kusa;
 
-import java.util.TimerTask;
-import java.util.List;
+
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
 import com.kusa.service.GDriveService;
+import com.kusa.service.LocalService;
+
+import java.util.TimerTask;
+import java.util.List;
+import java.util.Set;
 
 
 public class VideoDownloaderTask extends TimerTask {
@@ -19,9 +23,12 @@ public class VideoDownloaderTask extends TimerTask {
   {
     System.out.println("VideoDownloader downloading vids...");
     List<File> videos = gds.getVideoFiles();
+    Set<String> localVideos = LocalService.getVideoMRLS();
     for(File video : videos)
     {
-      gds.downloadFile(video.getId(), "videos/");
+      String p = LocalService.videosPath + video.getName();
+      if(!localVideos.contains(p))
+        gds.downloadFile(video.getId(), "videos/");
     }
   }
 }
