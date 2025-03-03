2ad1e77762149a69d88731ad340a6c2b1a235d6e
Kevin Barrios <kusakus@live.com>
2025-03-02 20:14:14 -0800

    formatting changes, we should prolly remove the auto creation of videos and photos folder

--- src/main/java/com/kusa/Config.java
+++ src/main/java/com/kusa/Config.java
@@ -11,100 +11,90 @@ import java.util.List;
 /**
  * Class for managing the applications configuration.
  *
- * an attempt to manage the internal directories seamlessly
+ * <p>an attempt to manage the internal directories seamlessly
  *
- * appPath - where the app will "install"
- *  currently platform specific might not work on windows.
+ * <p>appPath - where the app will "install" currently platform specific might not work on windows.
  */
-public class Config
-{
-  //this is platform specific. it should work fine on unix 
+public class Config {
+  // this is platform specific. it should work fine on unix
   private static final String appPath = System.getProperty("user.home") + "/.jinzo/";
   private static Properties props;
 
   /**
    * Hidden "Start Up".
    *
-   * this method will fire at the start of programs execution.
+   * <p>this method will fire at the start of programs execution.
    *
-   * we try to load in the application properties as well as verify
-   * that all needed directories exist.
+   * <p>we try to load in the application properties as well as verify that all needed directories
+   * exist.
    *
-   * we check for a relative application.properties first meaning if one exists
-   * in the src/main/resources folder it will be used. However,
-   * the main appPath will still be created! 
+   * <p>we check for a relative application.properties first meaning if one exists in the
+   * src/main/resources folder it will be used. However, the main appPath will still be created!
    *
-   * if any exceptions occur when trying to read or create files/directories that app
-   * will crash!
+   * <p>if any exceptions occur when trying to read or create files/directories that app will crash!
    *
-   * if certain properties for services are not found a warning should appear
-   * with steps on how to resolve it, and what service cannot be used.
+   * <p>if certain properties for services are not found a warning should appear with steps on how
+   * to resolve it, and what service cannot be used.
    */
   static {
-    try{
+    try {
       boolean propsLoaded = false;
 
       props = new Properties();
-      try{
-        //when developing you can have a defined properties file in the resources folder.
-        //it requires recompilation upon editing.
+      try {
+        // when developing you can have a defined properties file in the resources folder.
+        // it requires recompilation upon editing.
         props.load(Config.class.getResourceAsStream("/application.properties"));
         propsLoaded = true;
         System.out.println("Loaded local properties.");
-      }catch (Exception e){
+      } catch (Exception e) {
         System.out.println("Couldnt find local properties");
       }
 
       File appFolder = new File(appPath);
-      if(!appFolder.exists())
-        if(!appFolder.mkdirs())
-          System.out.println("FAILED TO FIND OR CREATE APP FOLDER.");
+      if (!appFolder.exists())
+        if (!appFolder.mkdirs()) System.out.println("FAILED TO FIND OR CREATE APP FOLDER.");
 
-      if(!propsLoaded)
-      {
+      if (!propsLoaded) {
         File propertyFile = new File(appPath + "application.properties");
-        if(!propertyFile.exists())
-        {
+        if (!propertyFile.exists()) {
           System.out.println("writing a default properties file.");
           Files.createFile(Paths.get(propertyFile.getAbsolutePath()));
           writeDefaultAppProperties(propertyFile);
         }
         props.load(new FileReader(propertyFile));
-        System.out.printf("[SUCSSESS] loaded properties file: %s\n", propertyFile.getAbsolutePath());
+        System.out.printf(
+            "[SUCSSESS] loaded properties file: %s\n", propertyFile.getAbsolutePath());
       }
 
       System.out.println(getProperty("downloadPath"));
       System.out.println(getProperty("tokenStoragePath"));
       System.out.println(getProperty("googleCredentialsPath"));
       File driveFolder = new File(getProperty("downloadPath"));
-      if(!driveFolder.exists())
-        if(!driveFolder.mkdirs())
-          System.out.println("FAILED TO FIND OR CREATE DRIVE FOLDER.");
+      if (!driveFolder.exists())
+        if (!driveFolder.mkdirs()) System.out.println("FAILED TO FIND OR CREATE DRIVE FOLDER.");
 
-      File videoFolder = new File(getProperty("downloadPath") +  "videos/");
-      if(!videoFolder.exists())
-        if(!videoFolder.mkdirs())
-          System.out.println("FAILED TO FIND OR CREATE VIDEO FOLDER.");
+      File videoFolder = new File(getProperty("downloadPath") + "videos/");
+      if (!videoFolder.exists())
+        if (!videoFolder.mkdirs()) System.out.println("FAILED TO FIND OR CREATE VIDEO FOLDER.");
 
       File photoFolder = new File(getProperty("downloadPath") + "photos/");
-      if(!photoFolder.exists())
-        if(!photoFolder.mkdirs())
-          System.out.println("FAILED TO FIND OR CREATE PHOTOS FOLDER.");
+      if (!photoFolder.exists())
+        if (!photoFolder.mkdirs()) System.out.println("FAILED TO FIND OR CREATE PHOTOS FOLDER.");
 
       File tokenFolder = new File(getProperty("tokenStoragePath"));
-      if(!tokenFolder.exists())
-        if(!tokenFolder.mkdirs())
-          System.out.println("FAILED TO FIND OR CREATE TOKEN FOLDER.");
-
+      if (!tokenFolder.exists())
+        if (!tokenFolder.mkdirs()) System.out.println("FAILED TO FIND OR CREATE TOKEN FOLDER.");
 
       File appCreds = new File(props.getProperty("googleCredentialsPath"));
-      if(!appCreds.exists())
-      {
-        System.out.println("WARNING!!! NO APP CREDENTIALS FOUND: " + props.getProperty("googleCredentialsPath") + " (DRIVE SERVICES WILL NOT WORK)!");
+      if (!appCreds.exists()) {
+        System.out.println(
+            "WARNING!!! NO APP CREDENTIALS FOUND: "
+                + props.getProperty("googleCredentialsPath")
+                + " (DRIVE SERVICES WILL NOT WORK)!");
       }
 
-    } catch (Exception e)
-    {
+    } catch (Exception e) {
       System.out.println("STARTUP FAILED!" + e);
       System.out.println(e.getMessage());
       e.printStackTrace();
@@ -115,44 +105,38 @@ public class Config
   /**
    * Returns the value of the property given the property name.
    *
-   * currently the app only has 3 valid properties
-   * - tokenStoragePath
-   * - googleCredentialsPath
-   * - downloadPath
+   * <p>currently the app only has 3 valid properties - tokenStoragePath - googleCredentialsPath -
+   * downloadPath
    *
-   * if the amount of properties remains small we can consider 
-   * making them all getters instead of a generic get property.
+   * <p>if the amount of properties remains small we can consider making them all getters instead of
+   * a generic get property.
    */
-  public static String getProperty(String propName) 
-  { 
+  public static String getProperty(String propName) {
     return ((props == null) ? "" : props.getProperty(propName));
   }
 
   /*
    * When the app is first installing it will look for appliaction.properties in the
-   * class path. if its not found it will look in our main install folder .jinzo/ 
+   * class path. if its not found it will look in our main install folder .jinzo/
    * if its not found there the app will attempt to create it and fill it with the
    * default properties specified below.
    *
    * ./jinzo
    *  - /drive/ <- main media folder.
-   *  - /tokens/ <- drive users access tokens. 
+   *  - /tokens/ <- drive users access tokens.
    *  - credentials.json <- client credentials needed to connect to google services.
    *
    * credentials.json can NOT be created for you. it will only define the property
    * with the value of it's default location.
    */
-  private static void writeDefaultAppProperties(File file) throws IOException
-  {
+  private static void writeDefaultAppProperties(File file) throws IOException {
     final String tokenStorage = "tokenStoragePath=" + appPath + "tokens/";
     final String googleCreds = "googleCredentialsPath=" + appPath + "credentials.json";
     final String downloadPath = "downloadPath=" + appPath + "drive/";
     List<String> properties = List.of(tokenStorage, googleCreds, downloadPath);
-    try{
-      Files.write(Paths.get(file.getAbsolutePath()), properties); 
-    }
-    catch(IOException ioException)
-    {
+    try {
+      Files.write(Paths.get(file.getAbsolutePath()), properties);
+    } catch (IOException ioException) {
       throw ioException;
     }
   }
