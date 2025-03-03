ee066f2c021c6a9cf623384a39d44facf25e49fe
Kevin Barrios <kusakus@live.com>
2025-03-02 20:07:25 -0800

    added check dir method and removed mrl functions for a more general function

--- src/main/java/com/kusa/service/LocalService.java
+++ src/main/java/com/kusa/service/LocalService.java
@@ -3,12 +3,15 @@ import com.kusa.Config;
 import java.io.File;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.List;
+
 
 public class LocalService
 {
 
   //aliases
-  public static final String mediaPath = Config.getProperty("downloadPath");
   public static final String videosPath = Config.getProperty("downloadPath") + "videos/";
   public static final String photosPath = Config.getProperty("downloadPath") + "photos/";
 
@@ -23,40 +26,51 @@ public class LocalService
     return file.exists();
   } 
 
-  /**
-   * Returns list of paths for all the downloaded videos in our applications media folder.
-   *
-   * @return set of absolute paths for each file in apps media/videos folder.
+  /** returns true if directory is valid.
+   * this method will attempt to create the directory if
+   * it doesn't exist.
+   * returns false if directory isn't usable.
    */
-  public static Set<String> getVideoMRLS()
+  public static boolean checkDir(String path)
   {
-    File dir = new File(videosPath);
-    File[] files = dir.listFiles();
-    Set<String> names = new HashSet<>();
-    if (files != null)
-    {
-      for(File file : files)
-      {
-        names.add(file.getAbsolutePath());
-      }
-    }
-    return names;
+    File dir = new File(path);
+    if(!dir.exists())
+      return dir.mkdirs();
+    return true;
   }
 
-  public static Set<String> getPhotoMRLS()
+  /**
+   * Returns set of paths for files in app folder to be used as MRLS in vlc.
+   *
+   * remember, the root is always the download path specified in the properties file.
+   *
+   * if no parameters are passed it will get all files recursivley starting from root.
+   *
+   * @param nestedFolders a nested path you wanna narrow the search too. i.e "videos/" will give you all files in videos folder.
+   * @param recursive get all the files even from nested directories (DEFAULT IS TRUE).
+   * @return set of absolute paths for each file in app folder.
+   */
+  public static Set<String> getLocalMRLS() { return getLocalMRLS("", true); }
+  public static Set<String> getLocalMRLS(String nestedFolders, boolean recursive)
   {
-    File dir = new File(photosPath);
-    File[] files = dir.listFiles();
-    Set<String> names = new HashSet<>();
-    if (files != null)
+    final String searchPath = Config.getProperty("downloadPath") + nestedFolders;
+    Queue<File> folders = new LinkedList<>(List.of(new File(searchPath)));
+    Set<String> mrls = new HashSet<>();
+    while(!folders.isEmpty())
     {
+      File dir = folders.poll();
+      if(!dir.exists())
+        continue;
+      File[] files = dir.listFiles();
       for(File file : files)
       {
-        names.add(file.getAbsolutePath());
+        if(file.isDirectory() && recursive)
+          folders.add(file);
+
+        if(!file.isDirectory())
+          mrls.add(file.getAbsolutePath());
       }
     }
-    return names;
+    return mrls;
   }
-
-
 }
