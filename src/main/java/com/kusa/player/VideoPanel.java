5e77c7c271288a23e1f84142d9626ffde73db8fd
Kevin Barrios <kusakus@live.com>
2025-03-07 05:49:23 -0800

    make the video panel update the playlist if needed after each play

--- src/main/java/com/kusa/player/VideoPanel.java
+++ src/main/java/com/kusa/player/VideoPanel.java
@@ -6,10 +6,17 @@ import uk.co.caprica.vlcj.player.base.MediaPlayer;
 import uk.co.caprica.vlcj.player.base.events.MediaPlayerEvent;
 import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
 import com.kusa.service.LocalService;
+import com.kusa.service.GDriveService;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 import java.util.Timer;
 import com.kusa.playlist.Playlist;
+import java.time.LocalDateTime;
+import java.time.DayOfWeek;
+import com.kusa.util.PathedFile;
+import com.kusa.Config;
 
 
 /**
@@ -21,6 +28,9 @@ import com.kusa.playlist.Playlist;
 public class VideoPanel extends EmbeddedMediaPlayerComponent 
 {
   private Playlist playlist;
+
+  //gds is injected to the video panel but currently we don't use it.
+  private GDriveService gds;
   
   /**
    * Constructs a video panel for use in an engagment frame.
@@ -29,10 +39,12 @@ public class VideoPanel extends EmbeddedMediaPlayerComponent
    * class eventually so we don't have to manage the playlist 
    * within a video panel.
    */
-  public VideoPanel(Playlist playlist_)
+  public VideoPanel(Playlist playlist_, GDriveService gds_)
   {
     super();
     playlist = playlist_;
+    gds = gds_;
+
     setOpaque(true);
 
     //we setup a event adapter to automatically play the next video in a playlist
@@ -45,6 +57,38 @@ public class VideoPanel extends EmbeddedMediaPlayerComponent
           System.out.println("WARNING THE VIDEO PLAYER CURRENTLY HAS AN EMPTY PLAYLIST");
 
         System.out.println("A VIDEO FINISHED.");
+        Set<String> driveMrls = new HashSet<>();
+
+        boolean willShuffle = false;
+        if(playlist.index() == 0)
+        {
+          System.out.println("PLAYLIST OVER CLEARING THE PLAYLIST");
+          playlist.clear();
+          willShuffle = true;
+        }
+
+        Set<String> mrls = videoMRLS();
+        Set<String> currentTracks = new HashSet<>(playlist.trackList());
+        for(String video : mrls)
+        {
+          if(!currentTracks.contains(video))
+          {
+            System.out.println("ADDING NEW " + video + " TO PLAYLIST");
+            playlist.add(video);
+          }
+        }
+
+        if(willShuffle)
+        {
+          System.out.println("SHUFFLING PLAYLIST");
+          playlist.shuffle();
+
+          System.out.println("PLAYING NEW PLAYLIST! INDEX: " + playlist.index());
+          List<String> tracks = playlist.trackList();
+          for(int i = 0; i < tracks.size(); i++)
+            System.out.println(" " + i + tracks.get(i));
+        }
+
         final int idx = playlist.index();
         final String mrl = playlist.next();
 
@@ -71,4 +115,39 @@ public class VideoPanel extends EmbeddedMediaPlayerComponent
     mediaPlayer().media().play(mrl);
     System.out.printf("NOW STARTING %d : %s \n", idx, mrl);
   }
+
+  //returns mrls for all files in the videos directory
+  //and files in the corresponding day of week folder.
+  public static Set<String> videoMRLS()
+  {
+    Set<String> mrls = LocalService.getLocalMRLS("videos/", false);
+    switch(LocalDateTime.now().getDayOfWeek())
+    {
+      case MONDAY:
+        mrls.addAll(LocalService.getLocalMRLS("videos/monday/", true));
+        break;
+      case TUESDAY:
+        mrls.addAll(LocalService.getLocalMRLS("videos/tuesday/", true));
+        break;
+      case WEDNESDAY:
+        mrls.addAll(LocalService.getLocalMRLS("videos/wednesday/", true));
+        break;
+      case THURSDAY:
+        mrls.addAll(LocalService.getLocalMRLS("videos/thursday/", true));
+        break;
+      case FRIDAY:
+        mrls.addAll(LocalService.getLocalMRLS("videos/friday/", true));
+        break;
+      case SATURDAY:
+        mrls.addAll(LocalService.getLocalMRLS("videos/saturday/", true));
+        break;
+      case SUNDAY:
+        mrls.addAll(LocalService.getLocalMRLS("videos/sunday/", true));
+        break;
+      default:
+        break;
+    }
+    return mrls;
+  }
+
 }
