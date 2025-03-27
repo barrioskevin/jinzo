d99b772fddce1c949426ca0d082cb7e6f6682536
Kevin Barrios <kusakus@live.com>
2025-03-27 14:09:19 -0700

    video panel interface, contains shared sttatic methods

--- src/main/java/com/kusa/player/VideoPanel.java
+++ src/main/java/com/kusa/player/VideoPanel.java
@@ -1,130 +1,25 @@
 package com.kusa.player;
 
-import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
-import uk.co.caprica.vlcj.player.component.CallbackMediaPlayerComponent;
-import uk.co.caprica.vlcj.player.base.MediaPlayer;
-import uk.co.caprica.vlcj.player.base.events.MediaPlayerEvent;
-import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
+//panel reference
+import javax.swing.JPanel;
+
+//retreiving videos
 import com.kusa.service.LocalService;
-import com.kusa.service.GDriveService;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.Set;
-import java.util.HashSet;
-import java.util.Timer;
-import com.kusa.playlist.Playlist;
 import java.time.LocalDateTime;
 import java.time.DayOfWeek;
-import com.kusa.util.PathedFile;
-import com.kusa.Config;
 
+//for calculatinng scale
+import java.awt.Dimension;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
-/**
- * Class representing the video content in an engagment frame.
- *
- * this class extends vlcjs embedded media player component for
- * access to vlc bindings.
- */
-public class VideoPanel extends EmbeddedMediaPlayerComponent 
-{
-  private Playlist playlist;
+public interface VideoPanel {
 
-  //gds is injected to the video panel but currently we don't use it.
-  private GDriveService gds;
-  
-  /**
-   * Constructs a video panel for use in an engagment frame.
+  /** 
+   *  Returns paths of all videos to play.
    *
-   * @param playlist_ the playlist dictating the media panel will play.
-   * @param gds_ the apps google drive service.
-   */
-  public VideoPanel(Playlist playlist_, GDriveService gds_)
-  {
-    super();
-    playlist = playlist_;
-    gds = gds_;
-
-    setOpaque(true); //maybe remove?
-
-    //we setup a event adapter to automatically play the next video in a playlist
-    //once a video ends.
-    mediaPlayer().events().addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
-      @Override
-      public void finished(MediaPlayer mp)
-      {
-        if(playlist.isEmpty())
-          log("WARNING the video panel playlist is empty!!!");
-
-        log("a video just finished.");
-        Set<String> driveMrls = new HashSet<>();
-
-        boolean willShuffle = false;
-        if(playlist.index() == 0)
-        {
-          log("playlist ended! calling clear...");
-          playlist.clear();
-          willShuffle = true;
-        }
-
-        Set<String> mrls = videoMRLS();
-        Set<String> currentTracks = new HashSet<>(playlist.trackList());
-        for(String video : mrls)
-        {
-          if(!currentTracks.contains(video))
-          {
-            log(String.format("found new video! adding %s to playlist.", video));
-            playlist.add(video);
-          }
-        }
-
-        if(willShuffle)
-        {
-          log("Shuffling Playlist!");
-          playlist.shuffle();
-
-          log(String.format("Start of new playlist! Index = ", playlist.index()));
-          List<String> tracks = playlist.trackList();
-          for(int i = 0; i < tracks.size(); i++)
-            log(String.format(" [%d] : %s", i, tracks.get(i)));
-        }
-
-        final int idx = playlist.index();
-        final String mrl = playlist.next();
-
-        mediaPlayer().submit(() -> mediaPlayer().media().play(mrl, "--avcodec-hw=mmal", "--no-xlib", "--no-osd", "--no-interact", "--no-video-filter", "--quiet"));
-
-        log(String.format("Now playing:\n [index]%d\n [vid]:%s", idx, mrl));
-        log(String.format("Next index should be %d", playlist.index()));
-      }
-    });
-  }
-
-  /**
-   * Advances the playlist and plays the next media.
-   * expects the media player to be <strong>Stopped.</strong>
-   *
-   * called by the main app to start the video panel. 
-   *
-   * because the video panel takes care of starting new videos 
-   * after one ends this method should only be called once 
-   * after the engagment frame has been created.
-   */
-  public void play()
-  {
-    final int idx = playlist.index();
-    final String mrl = playlist.next();
-    mediaPlayer().media().play(mrl);
-    log(String.format("Starting video panel.\n [index]:%d\n [vid]:%s", idx, mrl));
-  }
-
-
-  
-  /** Returns mrls for all files in the videos directory
-   *  and files in the corresponding day of week folder.
-   *
-   *  this function is kinda specific. it's basically the videos 
-   *  you want to consider for the playlist. the playlist will call
-   *  this method and add any mrls it doesn't have.
+   *  returs all files in videos and checks day of week.
    */
   public static Set<String> videoMRLS()
   {
@@ -158,9 +53,28 @@ public class VideoPanel extends EmbeddedMediaPlayerComponent
     return mrls;
   }
 
-  private void log(String message)
+  //attempts to find a correct scale to ensure the container
+  //will be filled with video content.
+  //
+  //this is likely to crop and zoom the video.
+  public static float calcScale(Dimension videoDimension, Dimension containerDimension)
   {
-    System.out.println("[VideoPanel] " + message);
+    float ws = (float)containerDimension.width / (float)videoDimension.width;
+    BigDecimal bd = new BigDecimal(Float.toString(ws));
+    bd = bd.setScale(2, RoundingMode.HALF_UP);
+    final float widthScale = bd.floatValue();
+
+    float hs = (float)containerDimension.height / (float)videoDimension.height;
+    BigDecimal bd2 = new BigDecimal(Float.toString(hs));
+    bd2 = bd2.setScale(2, RoundingMode.HALF_UP);
+    final float heightScale = bd2.floatValue();
+
+    return Math.max(widthScale, heightScale);
   }
 
+
+  void start();
+  void stop();
+  void shutdown();
+  JPanel panel();
 }
