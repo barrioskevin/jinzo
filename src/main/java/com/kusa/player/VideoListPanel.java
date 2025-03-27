08463438dfba883534a324c4d8dd5321c95461b3
Kevin Barrios <kusakus@live.com>
2025-03-27 14:08:52 -0700

    refactor video list panel to implement new interface + clean up imports

--- src/main/java/com/kusa/player/VideoListPanel.java
+++ src/main/java/com/kusa/player/VideoListPanel.java
@@ -1,31 +1,18 @@
 package com.kusa.player;
 
-import uk.co.caprica.vlcj.player.component.EmbeddedMediaPlayerComponent;
+import javax.swing.JPanel;
 import uk.co.caprica.vlcj.player.component.EmbeddedMediaListPlayerComponent;
-import uk.co.caprica.vlcj.player.component.CallbackMediaPlayerComponent;
 import uk.co.caprica.vlcj.player.base.MediaPlayer;
 import uk.co.caprica.vlcj.player.list.MediaListPlayer;
 import uk.co.caprica.vlcj.media.MediaRef;
 import uk.co.caprica.vlcj.media.TrackType; 
 import uk.co.caprica.vlcj.medialist.MediaList;
-import uk.co.caprica.vlcj.player.base.events.MediaPlayerEvent;
-import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
-import com.kusa.service.LocalService;
+
 import com.kusa.service.GDriveService;
-import java.util.List;
-import java.util.ArrayList;
+import com.kusa.playlist.Playlist;
+
 import java.util.Set;
 import java.util.HashSet;
-import java.util.Timer;
-import com.kusa.playlist.Playlist;
-import java.time.LocalDateTime;
-import java.time.DayOfWeek;
-import com.kusa.util.PathedFile;
-import com.kusa.Config;
-
-import java.awt.Dimension;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 /**
  * Class representing the video content in an engagment frame.
@@ -33,7 +20,7 @@ import java.math.RoundingMode;
  * this class extends vlcjs embedded media list player component for
  * access to vlc bindings.
  */
-public class VideoListPanel extends EmbeddedMediaListPlayerComponent 
+public class VideoListPanel extends EmbeddedMediaListPlayerComponent implements VideoPanel 
 {
   //tracks local files to play.
   private Playlist playlist;
@@ -61,22 +48,39 @@ public class VideoListPanel extends EmbeddedMediaListPlayerComponent
       mediaListPlayer().list().media().add(video);
   }
 
+  /**
+   * This event gets triggered when any new media starts to play.
+   *
+   * We make sure set the scale accordingly, log the current video,
+   * and advance the playlist.
+   */
   @Override
   public void playing(MediaPlayer mp)
   {
     //ensure correct scaling for frame based on video and container size.
-    mp.video().setScale(calcScale(mp.video().videoDimension(), getSize()));
+    mp.video().setScale(VideoPanel.calcScale(mp.video().videoDimension(), getSize()));
 
     log(String.format("Now playing...\n [vid] %s\n [idx] %d", playlist.current(), playlist.index())); 
     playlist.next();
   }
 
+  /**
+   * This event gets triggered when the player reaches the end of playlist.
+   *
+   * We clear the playlist and the players playlist.
+   *
+   * add new videos from 'videoMRLS' to local playlist
+   *
+   * shuffle playlist
+   * add all the new media to the player.
+   * call play on first index.
+   */
   @Override
   public void mediaListPlayerFinished(MediaListPlayer mlp)
   {
     playlist.clear();
     mediaListPlayer().submit(() -> mediaListPlayer().list().media().clear());
-    for(String video : videoMRLS())
+    for(String video : VideoPanel.videoMRLS())
       playlist.add(video);
 
     playlist.shuffle();
@@ -88,13 +92,18 @@ public class VideoListPanel extends EmbeddedMediaListPlayerComponent
     log(String.format("Starting new playlist of %s videos.", playlist.size()));
   }
 
+  /**
+   * This event gets triggered after a new item in the media list is played.
+   *
+   * We make a call to videoMRLS after each video to see if we need to add anything.
+   */
   @Override
   public void nextItem(MediaListPlayer mlp, MediaRef ref)
   {
     //add any new found tracks to playlist and media list.
     //TODO could be inefficient to perform this after each item.
     Set<String> currentTracks = new HashSet<>(playlist.trackList());
-    for(String video : videoMRLS())
+    for(String video : VideoPanel.videoMRLS())
     {
       if(!currentTracks.contains(video))
       {
@@ -111,72 +120,36 @@ public class VideoListPanel extends EmbeddedMediaListPlayerComponent
    * expects the media list player to be <strong>Stopped.</strong>
    *
    * called by the main app to start the video panel. 
+   *
+   * maybe we should force stop?
    */
-  public void play()
+  @Override
+  public void start()
   {
     log(String.format("Starting new playlist of %s videos.", playlist.size()));
-    log("wrapper play() called.");
+    log("START called.");
     mediaListPlayer().controls().play();
   }
 
-  //attempts to find a correct scale to ensure the container
-  //will be filled with video content.
-  //
-  //this is likely to crop and zoom the video.
-  private float calcScale(Dimension videoDimension, Dimension containerDimension)
+  //stops the player.
+  @Override
+  public void stop()
   {
-    float ws = (float)containerDimension.width / (float)videoDimension.width;
-    BigDecimal bd = new BigDecimal(Float.toString(ws));
-    bd = bd.setScale(2, RoundingMode.HALF_UP);
-    final float widthScale = bd.floatValue();
+    mediaListPlayer().submit(() -> mediaListPlayer().controls().stop());
+  }
 
-    float hs = (float)containerDimension.height / (float)videoDimension.height;
-    BigDecimal bd2 = new BigDecimal(Float.toString(hs));
-    bd2 = bd2.setScale(2, RoundingMode.HALF_UP);
-    final float heightScale = bd2.floatValue();
+  @Override
+  public void shutdown()
+  {
+    this.release();
+  }
 
-    return Math.max(widthScale, heightScale);
+  @Override
+  public JPanel panel()
+  {
+    return (JPanel) this;
   }
   
-  /** Returns mrls for all files in the videos directory
-   *  and files in the corresponding day of week folder.
-   *
-   *  this function is kinda specific. it's basically the videos 
-   *  you want to consider for the playlist. the playlist will call
-   *  this method and add any mrls it doesn't have.
-   */
-  public static Set<String> videoMRLS()
-  {
-    Set<String> mrls = LocalService.getLocalMRLS("videos/", false);
-    switch(LocalDateTime.now().getDayOfWeek())
-    {
-      case MONDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/monday/", true));
-        break;
-      case TUESDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/tuesday/", true));
-        break;
-      case WEDNESDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/wednesday/", true));
-        break;
-      case THURSDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/thursday/", true));
-        break;
-      case FRIDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/friday/", true));
-        break;
-      case SATURDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/saturday/", true));
-        break;
-      case SUNDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/sunday/", true));
-        break;
-      default:
-        break;
-    }
-    return mrls;
-  }
-
   private void log(String message)
   {
     System.out.println("[VideoListPanel] " + message);
