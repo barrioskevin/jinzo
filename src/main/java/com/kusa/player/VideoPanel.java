39d123e9144b561fb79a0e1f8ede2b2227c4a3e2
Kevin Barrios <kusakus@live.com>
2025-04-05 19:12:00 -0700

    updated the static get video method to read videos from the latest playlist

--- src/main/java/com/kusa/player/VideoPanel.java
+++ src/main/java/com/kusa/player/VideoPanel.java
@@ -4,8 +4,11 @@ package com.kusa.player;
 import javax.swing.JPanel;
 
 //retreiving videos
+import com.kusa.Config;
 import com.kusa.service.LocalService;
+import com.kusa.util.PlaylistFile;
 import java.util.Set;
+import java.util.HashSet;
 import java.time.LocalDateTime;
 import java.time.DayOfWeek;
 
@@ -14,49 +17,54 @@ import java.awt.Dimension;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+//exceptions ?!
+import java.io.IOException;
+
 public interface VideoPanel {
 
-  /** 
-   *  Returns paths of all videos to play.
+  /**
+   * Will return the latest videos based on the configured playlist file.
    *
-   *  returs all files in videos and checks day of week.
+   * the playlist file is what makes up where we grab the videos from.
+   *  see PlaylistFile.latest()
+   *
+   * the file seperates content into sections, there are special sections
+   * corresponding to the days of the week which will handle playing
+   * specific videos on specific days.
+   *
+   * @return set of video paths from latest playlist or empty if playlist failed to read.
    */
   public static Set<String> videoMRLS()
   {
-    Set<String> mrls = LocalService.getLocalMRLS("videos/", false);
-    switch(LocalDateTime.now().getDayOfWeek())
-    {
-      case MONDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/monday/", true));
-        break;
-      case TUESDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/tuesday/", true));
-        break;
-      case WEDNESDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/wednesday/", true));
-        break;
-      case THURSDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/thursday/", true));
-        break;
-      case FRIDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/friday/", true));
-        break;
-      case SATURDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/saturday/", true));
-        break;
-      case SUNDAY:
-        mrls.addAll(LocalService.getLocalMRLS("videos/sunday/", true));
-        break;
-      default:
-        break;
+    try {
+      //Config.playlistFiles()[0]
+      // could be a property to have panel choose a playlist
+      PlaylistFile myPlaylist = new PlaylistFile(Config.playlistFiles()[0]);
+      return new HashSet<String>(myPlaylist.latest().trackList());
+    } catch (IOException ioEx) {
+      ioEx.printStackTrace();
+    } catch (Exception x) {
+      //out of bounds execption.
+      //maybe default to other existing playlists?
+      x.printStackTrace();
     }
-    return mrls;
+    return new HashSet<String>();
   }
 
-  //attempts to find a correct scale to ensure the container
-  //will be filled with video content.
-  //
-  //this is likely to crop and zoom the video.
+  /**
+   * Attempts to find a correct scale to ensure the container will
+   * be filled with video content.
+   *
+   * either stretches until ends touch the horizontal border
+   * or until the ends touch the vertical border
+   *
+   * !! a high scale is likely to crop and zoom the video. !!
+   *
+   * @param videoDimension the dimensions of video that needs to be scaled.
+   * @param containerDimension the dimensions of container we want to fit video in.
+   *
+   * @return float - scale needed to zoom video to fit container's dimensions.
+   */
   public static float calcScale(Dimension videoDimension, Dimension containerDimension)
   {
     float ws = (float)containerDimension.width / (float)videoDimension.width;
