1fb1029dc15b43867115706c7cd114b34040ac27
Kevin Barrios <kusakus@live.com>
2025-04-13 11:22:21 -0700

    updated socket to have a timeout. control socket will only exist as long as vlcjapp is running

--- src/main/java/com/kusa/App.java
+++ src/main/java/com/kusa/App.java
@@ -1,12 +1,12 @@
 package com.kusa;
 
 import com.kusa.service.GDriveService;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.net.SocketTimeoutException;
 
 /**
  * Main class.
@@ -28,17 +28,20 @@ public class App {
 
     VlcjApp.exec(gds);
 
-    try(ServerSocket server = new ServerSocket(9999))
-    {
+    try (ServerSocket server = new ServerSocket(9999)) {
+      server.setSoTimeout(1000); //1 sec timeout.
       System.out.println("[SERVER] starting local server...");
-      while(true) {
-        Socket client = server.accept();
-        BufferedReader br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        String command = br.readLine();
-        System.out.println("command: " + command);
-        if(command.equals("pause"))
-          VlcjApp.pause();
-        client.close();
+      while (VlcjApp.running) {
+        try {
+          Socket client = server.accept();
+          BufferedReader br = new BufferedReader(
+            new InputStreamReader(client.getInputStream())
+          );
+          String command = br.readLine();
+          System.out.println("command: " + command);
+          if (command.equals("pause")) VlcjApp.pause();
+          client.close();
+        } catch (SocketTimeoutException ste) {}
       }
     } catch (IOException e) {
       e.printStackTrace();
