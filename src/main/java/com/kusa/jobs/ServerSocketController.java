b0e4ed73b56b6e1529b6e5ee51a48ea4a48c0b5b
Kevin Barrios <kusakus@live.com>
2025-04-13 20:05:58 -0700

    started to add more commands to controller,

--- src/main/java/com/kusa/jobs/ServerSocketController.java
+++ src/main/java/com/kusa/jobs/ServerSocketController.java
@@ -2,11 +2,14 @@ package com.kusa.jobs;
 
 import com.kusa.JinzoApp;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.List;
 
 /**
  * An attempt to control a JinzoApp by accepting commands
@@ -39,9 +42,14 @@ public class ServerSocketController implements Runnable {
           BufferedReader br = new BufferedReader(
             new InputStreamReader(client.getInputStream())
           );
-          handleCommand(br.readLine());
+          handleCommand(br.readLine(), client);
           client.close();
-        } catch (SocketTimeoutException ste) {}
+        } catch (SocketTimeoutException ste) {
+          //nothing
+        } catch (IOException ioEx) {
+          log("failed to communicate with client via buffered writier");
+          ioEx.printStackTrace();
+        }
       }
     } catch (IOException e) {
       e.printStackTrace();
@@ -50,9 +58,22 @@ public class ServerSocketController implements Runnable {
 
   //add new commands here!
   //
+  //  when i say returns i basically mean that
+  //  the whatever is being returned is actually
+  //  being written to the socket's outputstream.
+  //
   //  - status ()
   //  - pause (toggles pause on player.)
-  private void handleCommand(String command) {
+  //  - stop (closes player.)
+  //  - playing? (returns a string with status on the current playing media.)
+  //  - tracklist? (returns the complete video playlist inside jinzo app.)
+  //  - left? (returns status about jinzo app left panel.)
+  //  - right? (returns status about jinzo app right panel.)
+  private void handleCommand(String command, Socket client) throws IOException {
+    BufferedWriter writer = new BufferedWriter(
+      new OutputStreamWriter(client.getOutputStream())
+    );
+
     log("recieved command (raw): " + command);
     switch (command) {
       case "pause":
@@ -66,6 +87,28 @@ public class ServerSocketController implements Runnable {
         //this eventually stops this runnable also.
         jinzoApp.stop();
         break;
+      case "playing?":
+      case "PLAYING?":
+        log("playing? command triggered");
+        writer.write(getPlayingMessage("video"));
+        writer.flush();
+        break;
+      case "tracklist?":
+      case "TRACKLIST?":
+        log("tracklist? command triggered");
+        writer.write(getTracklistMessage());
+        writer.flush();
+        break;
+      case "left?":
+        log("left? command triggered");
+        writer.write(getPlayingMessage("left"));
+        writer.flush();
+        break;
+      case "right?":
+        log("right? command triggered");
+        writer.write(getPlayingMessage("right"));
+        writer.flush();
+        break;
       default:
         log("ignoring command: " + command);
         break;
@@ -74,5 +117,48 @@ public class ServerSocketController implements Runnable {
 
   private void log(String message) {
     System.out.println("[ServerSocketController] " + message);
+  }
+
+  //calls current video on jinzo app and returns
+  //something we can send to the buffered writer.
+  private String getPlayingMessage(String panel) {
+    String media = "n/a";
+    int idx = -1;
+    //add indexes here!
+    if (panel.equals("left")) media = jinzoApp.currentLeftPanelMedia();
+    if (panel.equals("right")) media = jinzoApp.currentRightPanelMedia();
+    if (panel.equals("video")) {
+      media = jinzoApp.currentVideoPanelMedia();
+      idx = jinzoApp.currentVideoIndex();
+    }
+    return idx != -1
+      ? String.format(
+        "Jinzo App's %s panel is currently playing [%d] %s\n",
+        panel,
+        idx,
+        media
+      )
+      : String.format(
+        "Jinzo App's %s panel is currently playing %s\n",
+        panel,
+        media
+      );
+  }
+
+  //calls tracklist on jinzo app and returns something
+  //we can send to the BufferedWriter.
+  private String getTracklistMessage() {
+    if (!jinzoApp.isRunning()) return "Jinzo App is not currently running.\n";
+
+    List<String> tracks = jinzoApp.videoTrackList();
+    StringBuilder sb = new StringBuilder();
+    sb.append("Loading Jinzo App's TrackList...\n");
+    sb.append(String.format("Found %d tracks...\n", tracks.size()));
+    for (int i = 0; i < tracks.size(); i++) {
+      String track = tracks.get(i);
+      if (i == jinzoApp.currentVideoIndex()) sb.append("*Playing Now* ");
+      sb.append(String.format("[%d] %s\n", i, track));
+    }
+    return sb.toString();
   }
 }
