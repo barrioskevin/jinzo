53fd95aec9c49d010a7016b7f65811fd77ed2572
Kevin Barrios <kusakus@live.com>
2025-04-15 20:30:57 -0700

    refactor side panel class (now updates with image objects not paths)

--- src/main/java/com/kusa/jobs/UpdateSidePanel.java
+++ src/main/java/com/kusa/jobs/UpdateSidePanel.java
@@ -2,48 +2,79 @@ package com.kusa.jobs;
 
 import com.kusa.player.SidePanel;
 import com.kusa.playlist.Playlist;
+import com.kusa.util.PlaylistFile;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Set;
+import javax.imageio.ImageIO;
 
+/**
+ * Updates a side panels images based
+ * on a playlist (file).
+ */
 public class UpdateSidePanel implements Runnable {
 
   private String panelName;
   private SidePanel panel;
+  private PlaylistFile playlistFile;
   private Playlist playlist;
-  private boolean isLeft;
+  private int index;
 
-  public UpdateSidePanel(SidePanel panel, Playlist playlist, boolean isLeft) {
-    this.panel = panel;
-    this.playlist = playlist;
-    this.isLeft = isLeft;
-    this.panelName = isLeft ? "left" : "right";
+  public UpdateSidePanel(SidePanel panel, PlaylistFile playlistFile) {
+    this(panel, playlistFile, "Panel", 0);
   }
 
+  public UpdateSidePanel(
+    SidePanel panel,
+    PlaylistFile playlistFile,
+    String name,
+    int startingIndex
+  ) {
+    this.panelName = name;
+    this.panel = panel;
+    this.playlistFile = playlistFile;
+    this.playlist = Playlist.dailyPlaylist(playlistFile);
+    this.index = startingIndex;
+  }
+
+  //reloads the playlist file whenever playlist ends.
   @Override
   public void run() {
-    if (playlist.index() == 0) {
+    if (index == playlist.size()) {
       log("clearing playlist.");
+      index = 0;
 
       //the photos come from side panel static method.
-      Set<String> photos = SidePanel.photoMRLS(isLeft);
-      if (!photos.isEmpty()) {
-        playlist.clear();
-        log(
-          String.format("setting up new playlist for %s panel...", panelName)
-        );
-        for (String pic : photos) {
-          log(String.format("adding %s to %s panel playlist.", pic, panelName));
-          playlist.add(pic);
-        }
+      this.playlistFile.reload();
+      this.playlist = Playlist.dailyPlaylist(playlistFile);
+
+      log(
+        String.format(
+          "new side panel playlist loaded for %s\nin queue...",
+          panelName
+        )
+      );
+      for (int i = 0; i < playlist.size(); i++) {
+        String pic = playlist.trackList().get(i);
+        log(String.format(" [%d] %s", i, pic));
       }
     }
-    log(
-      String.format(
-        "set new image %s on %s panel",
-        playlist.current(),
-        panelName
-      )
-    );
-    panel.setImage(playlist.next());
+    //get next image (increments index)
+    try {
+      BufferedImage nextImage = ImageIO.read(
+        new File(playlist.trackAt(index++))
+      );
+      panel.setImage(nextImage);
+      log(String.format("set new image %s on %s panel", nextImage, panelName));
+    } catch (Exception ex) {
+      log(
+        String.format(
+          "failed to set the next image in %s side panel",
+          panelName
+        )
+      );
+    }
   }
 
   private void log(String message) {
--- src/main/java/com/kusa/player/SidePanel.java
+++ src/main/java/com/kusa/player/SidePanel.java
@@ -32,74 +32,53 @@ import org.imgscalr.Scalr;
  */
 public class SidePanel extends JPanel {
 
-  private BufferedImage originalImage;
-  private Image scaledImage;
+  private final int width;
+  private final int height;
+
+  private Image image;
   private ImageIcon icon;
   private JLabel imageLabel;
 
   /**
-   * Constructs a side panel with the default dimensions.
-   * default dimensions attempt to take up 1/3 of the 1080p display.
-   * be careful if your display is not 1080p the images might not show up as expected.
-   * @param path path of image content to be displayed.
+   * Returns an all black image with the width
+   * and height specified.
+   * @param width width of all black image.
+   * @param height height of all black image.
    */
-  public SidePanel(String path) {
-    this(path, 640, 1080);
+  public static Image blackImage(int width, int height) {
+    Image fallback = new BufferedImage(
+      width,
+      height,
+      BufferedImage.TYPE_INT_ARGB
+    );
+    Graphics g = fallback.getGraphics();
+    g.setColor(Color.BLACK);
+    g.fillRect(0, 0, width, height);
+    g.dispose();
+    return fallback;
+  }
+
+  public SidePanel(int width, int height) {
+    this(blackImage(width, height), width, height);
   }
 
   /**
    * Constucts a side panel with custom dimensions.
-   * we can specify our own width and height but it's up to the engament frame to
-   * determine how it will be displayed in the end.
-   * (this might not be true. the size should probably be set HERE)
-   * @param path path of image content to be displayed.
+   * we can specify our own width and height any images
+   * we set will be scaled to this width and height.
+   *
+   * @param image image content to be displayed.
    * @param width int representing width of side panel.
    * @param height int representing height of side panel.
    */
-  public SidePanel(String path, int width, int height) {
-    //layout (border layout might not be best)
+  public SidePanel(Image image, int width, int height) {
+    this.width = width;
+    this.height = height;
     setLayout(new BorderLayout());
-
-    /*
-     * LOADING IMAGE.
-     *
-     * if we fail to read the image this panel will just be black.
-     * im trying to come up with better ways to scale the image so that the side panels image still
-     * fits correctly on different resolutions. previously i had used get scaled instance, but it
-     * didn't seem to work as nicely when displaying on TVs with high res. I think it could've been
-     * a bug that made the images not look as nice, i was passing in the screens width for both width and height
-     * so although the bug is gone our new solutions utilizes Scalr library to scale based on the width which
-     * should be 1/3 of the display. Im currently testing out better methods to scale the image.
-     *
-     * previous scaling code: scaledImage = originalImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-     */
-    try {
-      originalImage = ImageIO.read(new File(path));
-      scaledImage = Scalr.resize(
-        originalImage,
-        Scalr.Method.BALANCED,
-        Scalr.Mode.FIT_EXACT,
-        width,
-        height
-      );
-    } catch (Exception e) {
-      System.out.println("[SidePanel] IMAGE " + path + " FAILED TO LOAD");
-      scaledImage = new BufferedImage(
-        width,
-        height,
-        BufferedImage.TYPE_INT_ARGB
-      );
-      Graphics g = scaledImage.getGraphics();
-      g.setColor(Color.BLACK);
-      g.fillRect(0, 0, width, height);
-      g.dispose();
-    }
-
-    icon = new ImageIcon(scaledImage);
+    icon = new ImageIcon(image);
     imageLabel = new JLabel(icon);
     imageLabel.setOpaque(true);
     imageLabel.setBackground(Color.BLACK);
-
     add(imageLabel, BorderLayout.CENTER);
   }
 
@@ -109,52 +88,30 @@ public class SidePanel extends JPanel {
    * if it fails to load the new image it will
    * just stick to the image it currently has.
    *
-   * we have a Task for using this function i just havent
-   * found the best place to use it.
+   * !! any images passed in are scaled to this
+   *    panels width and height !!
    *
-   * @param path path of new image to be displayed.
+   * @param newImage image to be set.
    */
-  public void setImage(String path) {
-    BufferedImage newImage;
-    Image newScaledImage;
-    int width = imageLabel.getIcon().getIconWidth();
-    int height = imageLabel.getIcon().getIconHeight();
-    try {
-      newImage = ImageIO.read(new File(path));
-      newScaledImage = Scalr.resize(
-        newImage,
-        Scalr.Method.BALANCED,
-        Scalr.Mode.FIT_EXACT,
-        width,
-        height
-      );
-      //newScaledImage = Scalr.resize(newImage, Scalr.Method.BALANCED, Scalr.Mode.FIT_TO_WIDTH, width);
-      ImageIcon newIcon = new ImageIcon(newScaledImage);
-      imageLabel.setIcon(newIcon);
-    } catch (Exception e) {
-      System.out.println("[SidePanel] FAILED TO SET NEW IMAGE: " + path);
-    }
+  public void setImage(BufferedImage newImage) {
+    Image newScaledImage = Scalr.resize(
+      newImage,
+      Scalr.Method.BALANCED,
+      Scalr.Mode.FIT_EXACT,
+      width,
+      height
+    );
+    imageLabel.setIcon(new ImageIcon(newScaledImage));
   }
 
   /**
-   * Returns set of latest photo mrls based on the configured playlist.
+   * Returns name of current image.
    *
-   * @param leftPanel true if we are reading the left panels playlist.
-   * @return set of mrls representing latest photos of playlist or empty if playlist file failed to read.
+   * TODO fix this!
+   *
+   * @return this panel's current mrl or empty string (if no image set).
    */
-  public static Set<String> photoMRLS(boolean leftPanel) {
-    try {
-      String filePath = (leftPanel)
-        ? Config.playlistFiles()[1]
-        : Config.playlistFiles()[2];
-      PlaylistFile playlistFile = new PlaylistFile(filePath);
-      return new HashSet<String>(playlistFile.latest().trackList());
-    } catch (IOException ioe) {
-      ioe.printStackTrace();
-    } catch (Exception ex) {
-      //out of bounds maybe.
-      ex.printStackTrace();
-    }
-    return new HashSet<String>();
+  public String currentMedia() {
+    return "WIP";
   }
 }
