5a0d2d782659bfb346a8fc017f1698a52d52e94b
Kevin Barrios <kusakus@live.com>
2025-04-15 20:27:26 -0700

    added support for http streams ? :)

--- src/main/java/com/kusa/util/PlaylistFile.java
+++ src/main/java/com/kusa/util/PlaylistFile.java
@@ -1,7 +1,7 @@
 package com.kusa.util;
 
-import com.kusa.playlist.ScalaPlaylistFactory;
 import com.kusa.playlist.Playlist;
+import com.kusa.playlist.ScalaPlaylistFactory;
 import com.kusa.service.LocalService;
 import java.io.File;
 import java.io.IOException;
@@ -25,8 +25,6 @@ import org.apache.commons.io.FileUtils;
  * a playlist file to com.kusa.playlist.Playlist object.
  */
 public class PlaylistFile {
-
-  
 
   private File playlistFile;
   private String path;
@@ -69,7 +67,7 @@ public class PlaylistFile {
 
   /**
    * Will attempt to read playlist from file and update
-   * the internal structures. 
+   * the internal structures.
    *
    * by succesful we mean
    * doesn't include any checks to see if there
@@ -85,7 +83,7 @@ public class PlaylistFile {
       sections.clear();
       generate();
       return true;
-    } catch (Exception ex) { ; }
+    } catch (Exception ex) {}
     return false;
   }
 
@@ -97,11 +95,12 @@ public class PlaylistFile {
    */
   public Playlist playlistFromSection(String section) {
     List<String> ret = new ArrayList<>();
-    if (sections.get(section) != null)
-      for(String content : sections.get(section))
-        ret.addAll(resolvePlaylistContent(content));
+    if (sections.get(section) != null) for (String content : sections.get(
+      section
+    )) ret.addAll(resolvePlaylistContent(content));
     return ScalaPlaylistFactory.simplePlaylist(ret);
   }
+
   public int sectionCount() {
     return sections.keySet().size();
   }
@@ -109,7 +108,7 @@ public class PlaylistFile {
   public Set<String> sectionNames() {
     return sections.keySet();
   }
-    
+
   /*
    * this method is part of the parsing process of .playlist files.
    * when reading each line under a section it could represent any of
@@ -117,11 +116,16 @@ public class PlaylistFile {
    *  - single file
    *  - a folder of files
    *  - a folder including all nested folders
+   *  - a direct link (http/https)
    *
    * we resolve that here to turn those representations into
    * valid mrls. (commonly absolute local file paths).
+   *
+   * ideally we want to make sure the video player could
+   * play the strings we return here.
    */
   private List<String> resolvePlaylistContent(String content) {
+    if (content.contains("http")) return List.of(content);
     String[] parts = content.split("/");
     if (parts[parts.length - 1].contains("*")) {
       String stars = parts[parts.length - 1];
